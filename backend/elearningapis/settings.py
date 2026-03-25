eeabc0ac26739f764e022bfea581d9679df96275
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-03-25 20:16:31 +0700

    feat(worker): setup Celery worker

--- backend/elearningapis/settings.py
+++ backend/elearningapis/settings.py
@@ -153,9 +153,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Email configuration
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+# Celery configuration
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
