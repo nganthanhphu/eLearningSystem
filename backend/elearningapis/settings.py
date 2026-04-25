f1570526d6a063f54115dd31fc7b638c5a706f3f
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-04-26 02:42:50 +0700

    fix: update configuration for django static files handling

--- backend/elearningapis/settings.py
+++ backend/elearningapis/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Email configuration
 
